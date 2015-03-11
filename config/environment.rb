8c8969b7319cade94116a3f06c7262bb545a1e3d
Maciej Malecki <smt116@gmail.com>
2015-03-11 20:32:33 +0100

    Use a single application instead of container.
    Default multi-app support may result in poorer performance, because of
    double routing.

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require 'bundler/setup'
 require 'lotus/setup'
 require_relative '../lib/lotus-application'
 require_relative '../apps/web/application'
-
-Lotus::Container.configure do
-  mount Web::Application, at: '/'
-end
